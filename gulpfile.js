41e056197093d06d68d3ad275556d018b8d24a31
Daniel <daniel.meter0@gmail.com>
2018-03-13 21:06:06 +0200

    added webpack compilation :))

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,36 @@
 const gulp = require('gulp');
+const path = require('path');
 const webpack = require('webpack');
-const gulpwebpack = require('gulp-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 gulp.task('default', (done)=>{
     done();
 });
-gulp.task('serve', (done) => {
-    return gulp.src('src/web/scripts/game.ts')
-    .pipe(gulpwebpack({
-        module: {
-          rules: [
-            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-            { test: /\.tsx?$/, loader: "ts-loader" }
-          ]
-        },
-      }))
-    .pipe(gulp.dest('dist/'));
+
+gulp.task('build', (done) => {
+    webpack({
+      entry: __dirname + '/src/web/scripts/game.ts',
+      output: {
+        path: __dirname + '/dist',
+        filename: 'index_bundle.js'
+      },
+      module: {
+        rules: [
+          // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+          { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+      },
+      plugins: [
+        new HtmlWebpackPlugin()
+      ]
+    }).run((err, stats) => {
+      console.log(__dirname + '/dist');
+      console.log(stats);
+      if (err) {
+        done(new Error(err));
+      }
+      else {
+        done();
+      }
+    });
 });
--- server.js
+++ server.js
@@ -1,2 +1,12 @@
-const app = socket(3000);
-const socket = require('socket.io');
+// const socket = require('socket.io');
+// const app = socket(3000);
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './src/web/index.html'));
+});
+
+app.listen(4000);
